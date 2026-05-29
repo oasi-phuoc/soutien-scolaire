cd125c83600e376e32143454ba3989014de5d360
Thuy-96 <tthuy.van@outlook.com>
2026-05-29 23:34:08 +0200

    fix: add useState import to VocabCards

--- components/francais/vocab/VocabCards.tsx
+++ components/francais/vocab/VocabCards.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, Fragment } from "react";
+import { useEffect, useState, Fragment } from "react";
 import type { VocabTheme, VocabTheoryBlock, VocabWord } from "@/lib/curriculum/vocabulary-data";
 import { playWord, SoundIcon } from "./vocabUtils";
 
